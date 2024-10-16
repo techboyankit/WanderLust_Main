e04fd9bc3175ff3d429a88432ca323bc28f9cf10
Ankit <sumanankitkr99@gmail.com>
2024-10-16 19:44:21 +0530

    NEW_COMMIT

---
+++ models/listing.js
@@ -0,0 +1,31 @@
+// Input elements (<input>, <textarea>, <select>)
+// Labels (<label>): Connect labels to form inputs to enhance accessibility.
+
+import { Schema as _Schema, model } from "mongoose";
+const Schema = _Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
+      // set empty string for default link
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = model("Listing", listingSchema);
+// module needs to be exported as well
+export default Listing;
